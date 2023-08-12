d4bcafb693a77d0d5d739759ec92965453c4108c
Michael Wiles <michaelwiles@afrozaar.com>
2023-08-12 10:19:31 +0200

    initial commit

---
+++ src/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import VBtn from "vuetify";
+</script>
+
+<template>
+  <v-btn text="hello" />
+  <!--  <button text="hello" />-->
+</template>
+
+<style scoped></style>
---
+++ src/setup.ts
@@ -0,0 +1,10 @@
+import { cleanup } from "@testing-library/vue"
+import matchers from "@testing-library/jest-dom/matchers"
+import { afterEach, expect } from "vitest"
+import "@testing-library/jest-dom"
+
+expect.extend(matchers)
+
+afterEach(() => {
+  cleanup()
+})
